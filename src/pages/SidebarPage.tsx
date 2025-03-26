766d3af5c8799d6f0a3ffc4c61aabd7a9b0f068b
clarahanna <clarahanna110@gmail.com>
2025-03-26 21:37:22 +0200

    b

--- src/pages/SidebarPage.tsx
+++ src/pages/SidebarPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../components/Sidebar"; 
 import { SidebarProps } from "../types/sidebarProps";
 
+
 const SidebarPage: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   return (
     <div>
